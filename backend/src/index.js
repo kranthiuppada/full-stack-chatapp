cc6cf8d364ebf270464e8ea151b5e17765d84ce9
kranthiuppada <kranthi.uppada123@gmail.com>
2025-11-02 17:53:57 +0530

    Fix: use /* instead of * for Express wildcard route

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,9 +32,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
-  })
+ app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+});
+
 
 }
 
